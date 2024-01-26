08d448dbbc9de6a1f2cced4ff7317302c8cb3ed7
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-01-26 23:19:30 +0100

    adds reduxToolKit, redux Persist, react-redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,88 @@
-import { useState, useEffect } from "react";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { ContactList } from "./ContactList/ContactList";
-import { Filter } from "./Filter/Filter";
-import { Section } from "./Section/Section";
-import { nanoid } from "nanoid";
+import { ContactForm, ContactList, Section, Filter } from 'components';
 
-export const App = () => {
-  // state = {
-  //   contacts: [],
-  //   filter: '',
-  //   name: '',
-  //   number: ''
-  // }
-
-  const [contacts, setContacts] = useState([]);
-  const [filter, setFilter] = useState('');
-  const [name, setName] = useState('')
-  const [number, setNumber] = useState('')
-
-  useEffect(() => {
-    const storedContacts = JSON.parse(localStorage.getItem('contacts'))
-    if (storedContacts && storedContacts.length > 0) {
-      setContacts(storedContacts)
-    }
-  }, []);
-  
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts))
-  }, [contacts])
-  
-  const onNameChange = e => {
-    setName(e.target.value)
-  }
-
-  const onNumberChange = e => {
-    setNumber(e.target.value)
-  }
-
-  const onFilterChange = e => {
-    setFilter(e.target.value)
-  }
-
-  const onSubmit = e => {
-    e.preventDefault()
-    const alreadyInContacts = contacts.some(contact => contact.name.toLowerCase() === name.trim().toLowerCase())
-    if (alreadyInContacts) {
-      alert(`Contact ${name} is already in List.`)
-      return;
-    }
-
-    const newContact = { id: nanoid(), name, number }
-    setContacts(prevContacts =>  [...prevContacts, newContact]
-    )
-    e.currentTarget.reset();
-  }
-
-  const onDeleteContact = idToDelete => {
-    const isConfirmed = window.confirm('Are you sure want to delete this contact?');
-    if (isConfirmed) {
-      setContacts(prevContacts => prevContacts.filter(contactElement => contactElement.id !== idToDelete))
-    }
-  }
-
-  const filteredContacts = contacts.filter(contactEl => contactEl.name.toLowerCase().includes(filter.trim().toLowerCase()))
-    
+export const App = () => {    
     return (
       <div>
         <Section title="Phonebook">
-          <ContactForm onNameChange={onNameChange} onNumberChange={onNumberChange} onSubmit={onSubmit}></ContactForm>
+          <ContactForm ></ContactForm>
         </Section>
         <Section title="Contacts">
-          <Filter filterValue ={filter} onFilterChange = {onFilterChange}></Filter>
-          <ContactList contactsList={filteredContacts} onDeleteContact = {onDeleteContact}></ContactList>
+          <Filter></Filter>
+          <ContactList></ContactList>
         </Section>
       </div>
-      
     )
 };
+
+
+// export const OLD App = () => {
+//   const dispatch = useDispatch();
+//   const state = useSelector(store => store)
+//   console.log(state)
+//   const contacts = useSelector(store => store.contacts.contacts);
+//   const filter = useSelector(store => store.contacts.filter);
+
+//   // const [contacts, setContacts] = useState([]);
+//   // const [filter, setFilter] = useState('');
+//   // const [name, setName] = useState('')
+//   // const [number, setNumber] = useState('')
+
+//   useEffect(() => {
+//     const storedContacts = JSON.parse(localStorage.getItem('contacts'))
+//     if (storedContacts && storedContacts.length > 0) {
+//       // setContacts(storedContacts)
+//     }
+//   }, []);
+  
+//   useEffect(() => {
+//     localStorage.setItem('contacts', JSON.stringify(contacts))
+//   }, [contacts])
+  
+//   const onNameChange = e => {
+//     setName(e.target.value)
+//   }
+
+//   const onNumberChange = e => {
+//     setNumber(e.target.value)
+//   }
+
+//   const onFilterChange = e => {
+//     setFilter(e.target.value)
+//   }
+
+//   const onSubmit = e => {
+//     e.preventDefault()
+//     const alreadyInContacts = contacts.some(contact => contact.name.toLowerCase() === name.trim().toLowerCase())
+//     if (alreadyInContacts) {
+//       alert(`Contact ${name} is already in List.`)
+//       return;
+//     }
+
+//     const newContact = { id: nanoid(), name, number }
+//     setContacts(prevContacts =>  [...prevContacts, newContact]
+//     )
+//     e.currentTarget.reset();
+//   }
+
+//   const onDeleteContact = idToDelete => {
+//     const isConfirmed = window.confirm('Are you sure want to delete this contact?');
+//     if (isConfirmed) {
+//       setContacts(prevContacts => prevContacts.filter(contactElement => contactElement.id !== idToDelete))
+//     }
+//   }
+
+//   const filteredContacts = contacts.filter(contactEl => contactEl.name.toLowerCase().includes(filter.trim().toLowerCase()))
+    
+//     return (
+//       <div>
+//         <Section title="Phonebook">
+//           <ContactForm onNameChange={onNameChange} onNumberChange={onNumberChange} onSubmit={onSubmit}></ContactForm>
+//         </Section>
+//         <Section title="Contacts">
+//           <Filter filterValue ={filter} onFilterChange = {onFilterChange}></Filter>
+//           <ContactList contactsList={filteredContacts} onDeleteContact = {onDeleteContact}></ContactList>
+//         </Section>
+//       </div>
+      
+//     )
+// };
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,18 @@
+import { useDispatch } from 'react-redux';
+import { saveFilterValue } from '../../redux/filter/filterReducer'
 import css from './Filter.module.css'
 
-export const Filter = ({ filterValue, onFilterChange }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+
+  const onFilterChange = (e) => {
+    const filterValue = e.target.value.trim();
+    dispatch(saveFilterValue(filterValue))
+  }
   return (
     <div className={css.filterWrapper}>
       <label className={css.filterLabel} htmlFor="filter">Find contacts by name</label>
-      <input className={css.filterInput}type="text" id="filter" name="filter" value={filterValue} onChange={onFilterChange} placeholder="John" />
+      <input className={css.filterInput}type="text" id="filter" name="filter" onChange={onFilterChange}  placeholder="John" />
     </div>
   )
 }
